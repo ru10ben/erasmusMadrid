e8f46a59eab1902fda22ab7f77b914c60926cadf
jorge9200 <jorge92f@gmail.com>
2015-03-17 21:49:27 +0100

    Path problem fixed

--- server.js
+++ server.js
@@ -11,26 +11,31 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-/*
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-*/
+//VIEW ENGINE SETUP
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use('/static' ,express.static(path.join(__dirname, '/static')));
+app.use(favicon(__dirname + '/static/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
 //Server
+/*
+app.get('/:nombre', function(req, res){
+    req.send('Hola' + req.params.nombre);
+});
+app.get('*', function(req, res){
+    
+
+});
+*/
 app.listen(1000);
 console.log("HOLA");
                                
